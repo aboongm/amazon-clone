5c4b904296a01601f94c5697f36c096fbbedca03
aboongm <aboongm@yahoo.com>
2022-09-06 21:24:28 +0530

    Add the linters

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,10 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: 'AIzaSyA4N2JnkXX-j3S0mB1DyKWA3Slmdj-5AVw',
-  authDomain: 'clone-fe1e7.firebaseapp.com',
-  projectId: 'clone-fe1e7',
-  storageBucket: 'clone-fe1e7.appspot.com',
-  messagingSenderId: '432456273291',
-  appId: '1:432456273291:web:f9fecbbfa44aa5bd4b738e',
-  measurementId: 'G-HFBQD3BNVW',
-};
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyA4N2JnkXX-j3S0mB1DyKWA3Slmdj-5AVw',
+//   authDomain: 'clone-fe1e7.firebaseapp.com',
+//   projectId: 'clone-fe1e7',
+//   storageBucket: 'clone-fe1e7.appspot.com',
+//   messagingSenderId: '432456273291',
+//   appId: '1:432456273291:web:f9fecbbfa44aa5bd4b738e',
+//   measurementId: 'G-HFBQD3BNVW',
+// };
