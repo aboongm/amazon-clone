78a3126c3080aa89b5f1c9504f783998007c5bda
aboongm <aboongm@yahoo.com>
2022-09-06 22:58:07 +0530

    Update App with header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <h1>Hi Ranjit!</h1>
+      {/* Header */}
+      {/* Home */}
     </div>
   );
 }
